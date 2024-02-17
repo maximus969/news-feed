a93de318b1266e2afb99aca1871f2270b73d4803
Maxim Bordea <maxbordea@gmail.com>
2024-02-17 21:32:45 +0200

    uncommmit changes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,18 @@
 import { Articles } from '../Articles/Articles'
 import { Article } from '../Article/Article'
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 
 import { Header } from '../Header/Header'
 import { Footer } from '../Footer/Footer'
 
 export const App: React.FC = () => {
-  // const { pathname } = useLocation()
+  const { pathname } = useLocation()
 
-  // useEffect(() => {
-  //   window.scrollTo(0, 0)
-  // }, [pathname])
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
 
   return (
     <React.Fragment>
